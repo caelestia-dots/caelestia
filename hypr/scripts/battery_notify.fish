408c8a4cc3f8c6fc72c6d32dbd90f0b1334709cc
GhostGamer6969 <arambhmani@hotmail.com>
2025-08-23 13:12:05 +0530

    update battery notifier to trigger at fixed thresholds

--- hypr/scripts/battery_notify.fish
+++ hypr/scripts/battery_notify.fish
@@ -1,13 +1,22 @@
 #!/usr/bin/env fish
 
+set last_capacity 100
+
 while true
     set capacity (cat /sys/class/power_supply/BAT0/capacity)
     set bat_status (cat /sys/class/power_supply/BAT0/status)
 
-    if test $capacity -le 15 -a "$bat_status" = "Discharging"
-        notify-send -u critical "Battery Low" "Battery at $capacity%"
+    if test "$bat_status" = "Discharging"
+        if test $capacity -le 15
+            set diff (math "$last_capacity - $capacity")
+            if test $capacity -eq 15 -o $capacity -eq 10 -o $capacity -eq 5 -o $capacity -eq 2 -o $capacity -eq 1 -o $diff -ge 5
+                notify-send -u critical "Battery Low" "Battery at $capacity%"
+                set last_capacity $capacity
+            end
+        end
+    else
+        set last_capacity 100
     end
 
     sleep 60
 end
-
