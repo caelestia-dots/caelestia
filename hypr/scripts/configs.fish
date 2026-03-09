c72fc2da476ae5da7ada4213c97d711d9c8b64ed
Dan Griffiths <dgriffiths@widgitlabs.com>
2026-03-09 05:53:31 -0500

    Safer handling for hyprland config validation
    Signed-off-by: Dan Griffiths <dgriffiths@widgitlabs.com>

--- hypr/scripts/configs.fish
+++ hypr/scripts/configs.fish
@@ -1,5 +1,7 @@
 #!/usr/bin/env fish
 
+set -l _reload false
+
 # Ensure config directory exists
 if ! test -d $argv
     mkdir -p $argv
@@ -8,9 +10,16 @@ end
 # Ensure hypr-vars exists
 if ! test -f $argv/hypr-vars.conf
     touch -a $argv/hypr-vars.conf
+    set -l _reload true
 end
 
 # Ensure hypr-user exists
 if ! test -f $argv/hypr-user.conf
     touch -a $argv/hypr-user.conf
+    set -l _reload true
+end
+
+# Reload as needed
+if _reload
+    hyprctl reload
 end
