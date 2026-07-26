e59e9e3cb40738345994804f744c0e2aa3e428d2
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2026-07-26 18:07:20 +1000

    chore: format

--- hypr/hyprland/execs.lua
+++ hypr/hyprland/execs.lua
@@ -31,16 +31,18 @@ end)
 
 -- Resizer listeners
 local function apply_resizer_rules(win)
-    local bitwarden_actions = {
+    local float_center = {
         hl.dsp.window.float({ action = "on", window = win }),
         hl.dsp.window.center({ window = win }),
     }
     local pip_actions = fn.move_actions(win) or {}
 
-    fn.resizer(win, "Bitwarden", 20, 54, bitwarden_actions, true, "class")
-    fn.resizer(win, "^Extension: %(Bitwarden Password Manager%) %- Bitwarden", 20, 54, bitwarden_actions, false)
-    -- Bitwarden Chromium extension ID
-    fn.resizer(win, "nngceckbapebfimnlniiiahkandclblb", 20, 54, bitwarden_actions, true, "class")
+    -- Bitwarden
+    fn.resizer(win, "Bitwarden", 20, 54, float_center, true, "class")                                       -- Native app
+    fn.resizer(win, "^Extension: %(Bitwarden Password Manager%) %- Bitwarden", 20, 54, float_center, false) -- Firefox
+    fn.resizer(win, "nngceckbapebfimnlniiiahkandclblb", 20, 54, float_center, true, "class")                -- Chromium
+
+    -- Picture in picture
     fn.resizer(win, "Picture[- ]in[- ][Pp]icture", 0, 0, pip_actions, false)
 end
 
